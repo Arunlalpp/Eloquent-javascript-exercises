20e48ac63a63f7c2bfe3b0a244e3dbaaaa1338ac
arunlalpp <arunlalpp@gmail.com>
2022-07-11 12:30:31 +0530

    Feat:Factorial

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-let total = 0,
-	count = 1;
-while (count <= 10) {
-	total += count;
-	count += 1;
+// let total = 0,
+// 	count = 1;
+// while (count <= 10) {
+// 	total += count;
+// 	count += 1;
+// }
+// console.log(total);
+/*....Factorial function */
+
+function factorial(n) {
+	if (n == 0) {
+		return 1;
+	} else {
+		return factorial(n - 1) * n;
+	}
 }
-console.log(total);
+console.log(factorial(8));
