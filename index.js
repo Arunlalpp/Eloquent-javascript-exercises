6f0f9140f67ff7fefe0a6888e0767c8d9133e5d7
arunlalpp <arunlalpp@gmail.com>
2022-07-18 22:00:13 +0530

    feat:chapter-3

--- index.js
+++ index.js
@@ -56,7 +56,6 @@ mood = "Dark";
 console.log(mood);
 let Benton = 140;
 
-
 Benton = Benton - 40;
 console.log(Benton);
 
@@ -155,7 +154,6 @@ for (let current = 20; ; current = current + 1) {
 //   }
 //   let n = 5;
 
-
 // External loop.
 // for (let i = 1; i <= n; i++) {
 // // printing spaces.
@@ -164,141 +162,210 @@ for (let current = 20; ; current = current + 1) {
 // }
 // }
 
-
 // Functins //
 
-const square = function(x) {
+const square = function (x) {
 	return x * x;
-  };
-  
-  console.log(square(12));
+};
 
+console.log(square(12));
 
-  const makeNoise = function() {
+const makeNoise = function () {
 	console.log("Pling!");
-  };
-  
+};
 
+//   makeNoise();
 
-  makeNoise();
-  
-  const power = function (base, exponent) {
-		let result = 1;
-		for (let count = 0; count < exponent; count++) {
-			result *= base;
-		}
-		return result;
-	};
+//   const power = function (base, exponent) {
+// 		let result = 1;
+// 		for (let count = 0; count < exponent; count++) {
+// 			result *= base;
+// 		}
+// 		return result;
+// 	};
 
-	console.log(power(2, 10));
+// 	console.log(power(2, 10));
 
-
-	let x = 10;
+let x = 10;
 if (true) {
-  let y = 20;
-  var z = 30;
-  console.log(x + y + z);
-  // → 60
+	let y = 20;
+	var z = 30;
+	console.log(x + y + z);
+	// → 60
 }
 // y is not visible here
 console.log(x + z);
 // → 40
 
-
-const halve = function(n) {
+const halve = function (n) {
 	return n / 2;
-  };
-  
-  let n = 10;
-  console.log(halve(100));
-  // → 50
-  console.log(n);
-  // → 10
+};
 
-  //Function in another Function //
+let n = 10;
+console.log(halve(100));
+// → 50
+console.log(n);
+// → 10
 
+//Function in another Function //
 
-  const hummus = function (factor) {
-		const ingredient = function (amount, unit, name) {
-			let ingredientAmount = amount * factor;
-			if (ingredientAmount > 1) {
-				unit += "s";
-			}
-			console.log(`${ingredientAmount} ${unit} ${name}`);
-		};
-		ingredient(1, "can", "chickpeas");
-		ingredient(0.25, "cup", "tahini");
-		ingredient(0.25, "cup", "lemon juice");
-		ingredient(1, "clove", "garlic");
-		ingredient(2, "tablespoon", "olive oil");
-		ingredient(0.5, "teaspoon", "cumin");
-	};
-
-	// let launchMissiles = function () {
-	// 	let safeMode;
-	// 	missileSystem.launch("now");
-	// };
-	// if (safeMode) {
-	// 	launchMissiles = function () {
-	// 		/* do nothing */
-	// 	};
-	// }
-
-	console.log("The future says:", future());
-
-	function future() {
-		return "You'll never have flying cars";
-	}
-
-	console.log("Arun", recret());
-
-	function recret() {
-		return "You will get soon";
-	}
-
-
-	const powerFull = (base, exponent) => {
-		let result = 1;
-		for (let count = 0; count < exponent; count++) {
-		  result *= base;
+const hummus = function (factor) {
+	const ingredient = function (amount, unit, name) {
+		let ingredientAmount = amount * factor;
+		if (ingredientAmount > 1) {
+			unit += "s";
 		}
-		return result;
-	  };
+		console.log(`${ingredientAmount} ${unit} ${name}`);
+	};
+	ingredient(1, "can", "chickpeas");
+	ingredient(0.25, "cup", "tahini");
+	ingredient(0.25, "cup", "lemon juice");
+	ingredient(1, "clove", "garlic");
+	ingredient(2, "tablespoon", "olive oil");
+	ingredient(0.5, "teaspoon", "cumin");
+};
 
+// let launchMissiles = function () {
+// 	let safeMode;
+// 	missileSystem.launch("now");
+// };
+// if (safeMode) {
+// 	launchMissiles = function () {
+// 		/* do nothing */
+// 	};
+// }
 
-	  const horn = () => {
-		console.log("Toot");
-	  };
+console.log("The future says:", future());
 
+function future() {
+	return "You'll never have flying cars";
+}
 
-	  function greet(who) {
-		console.log("Hello " + who);
-	  }
-	  greet("Harry");
-	  console.log("Bye");
+console.log("Arun", recret());
 
+function recret() {
+	return "You will get soon";
+}
 
-	  function chicken() {
-		return egg();
-	  }
-	  function egg() {
-		return chicken();
-	  }
-	  console.log(chicken() + " came first.");
-	  // → ??
+const powerFull = (base, exponent) => {
+	let result = 1;
+	for (let count = 0; count < exponent; count++) {
+		result *= base;
+	}
+	return result;
+};
 
+const horn = () => {
+	console.log("Toot");
+};
 
+//   function greet(who) {
+// 	console.log("Hello " + who);
+//   }
+//   greet("Harry");
+//   console.log("Bye");
 
-	  function minus(a, b) {
-		if (b === undefined) return -a;
-		else return a - b;
-	  }
-	  
-	  console.log(minus(10));
-	  // → -10
-	  console.log(minus(10, 5));
-	  // → 5
+//   function chicken() {
+// 	return egg();
+//   }
+//   function egg() {
+// 	return chicken();
+//   }
+//   console.log(chicken() + " came first.");
+//   // → ??
 
+function minus(a, b) {
+	if (b === undefined) return -a;
+	else return a - b;
+}
 
+console.log(minus(10));
+// → -10
+console.log(minus(10, 5));
+// → 5
 
+function power(base, exponent = 2) {
+	let result = 1;
+	for (let count = 0; count < exponent; count++) {
+		result *= base;
+	}
+	return result;
+}
 
+console.log(power(4));
+// → 16
+console.log(power(2, 6));
+// → 64
+
+console.log("C", "O", 2);
+// → C O 2
+
+function wrapValue(n) {
+	let local = n;
+	return () => local;
+}
+
+let wrap1 = wrapValue(1);
+let wrap2 = wrapValue(2);
+console.log(wrap1());
+console.log(wrap2());
+
+// function power(base, exponent) {
+// 	if (exponent == 0) {
+// 	  return 1;
+// 	} else {
+// 	  return base * power(base, exponent - 1);
+// 	}
+//   }
+
+//   console.log(power(2, 3));
+//   // → 8
+
+// function findSolution(target) {
+// 	function find(current, history) {
+// 		if (current == target) {
+// 			return history;
+// 		} else if (current > target) {
+// 			return null;
+// 		} else {
+// 			return (
+// 				find(current + 5, `(${history} + 5)`) ||
+// 				find(current * 3, `(${history} * 3)`)
+// 			);
+// 		}
+// 	}
+// 	return find(1, "1");
+// }
+
+// console.log(findSolution(24));
+// // → (((1 * 3) + 5) * 3)
+
+// function printFarmInventory(cows, chickens) {
+// 	let cowString = String(cows);
+// 	while (cowString.length < 3) {
+// 		cowString = "0" + cowString;
+// 	}
+// 	console.log(`${cowString} Cows`);
+// 	let chickenString = String(chickens);
+// 	while (chickenString.length < 3) {
+// 		chickenString = "0" + chickenString;
+// 	}
+// 	console.log(`${chickenString} Chickens`);
+// }
+// printFarmInventory(7, 11);
+
+// function printzeroPaddedwithLabel(number, label) {
+// 	let numberString = String(number);
+// 	while (numberString.length < 3) numberString = "0" + numberString;
+// 	{
+// 	}
+// 	console.log(`${numberString} ${label}`);
+// }
+
+const f = function (a) {
+	console.log(a + 2);
+};
+
+function g(a, b) {
+	return a * b * 3.14;
+}
