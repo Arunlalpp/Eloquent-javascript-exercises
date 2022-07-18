803bdcc6dcfd7da985858878bc09b2bbabf5bdf7
arunlalpp <arunlalpp@gmail.com>
2022-07-18 22:32:33 +0530

    feat:Chapeter-4

--- index.js
+++ index.js
@@ -369,3 +369,70 @@ const f = function (a) {
 function g(a, b) {
 	return a * b * 3.14;
 }
+
+//******Chapter-4 */
+
+//*****Arra */
+
+let listOfNumbers = [2, 3, 5, 7, 11];
+console.log(listOfNumbers[2]);
+// → 5
+console.log(listOfNumbers[0]);
+// → 2
+console.log(listOfNumbers[2 - 1]);
+// → 3
+
+let doh = "Doh";
+console.log(typeof doh.toUpperCase);
+// → function
+console.log(doh.toUpperCase());
+// → DOH
+
+let sequence = [1, 2, 3, 4];
+sequence.push(5);
+sequence.push(6);
+sequence.push(10);
+console.log(sequence);
+console.log(sequence.pop());
+
+let day1 = {
+	squirrel: false,
+	events: ["work", "touched tree", "pizza", "running"],
+};
+console.log(day1.squirrel);
+// → false
+console.log(day1.wolf);
+// → undefined
+day1.wolf = false;
+console.log(day1.wolf);
+// → false
+
+let anObject = { left: 1, right: 2 };
+console.log(anObject.left);
+// → 1
+delete anObject.left;
+console.log(anObject.left);
+// → undefined
+console.log("left" in anObject);
+// → false
+console.log("right" in anObject);
+// → true
+
+
+// Copy object fromn anpther object //
+
+let objectA = {a: 1, b: 2 ,c:8,d:12};
+Object.assign(objectA, {b: 3, c: 4,d:18});
+console.log(objectA);
+// → {a: 1, b: 3, c: 4}
+
+
+//Mutability//
+
+let object1 ={value: 10}
+let object2 ={value: 12}
+let object3 ={value: 14}
+let object4 ={value: 10}
+console.log(object1 == object4)
+
+console.log(object2 == object3)
